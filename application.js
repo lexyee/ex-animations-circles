c684995792172398bc045cee272b8b6329ade680
lexyee <leizhangcareer@gmail.com>
2016-10-03 20:25:44 +0800

    both changing

--- application.js
+++ application.js
@@ -9,9 +9,16 @@
 $(document).ready(function(){
 
 	$(document).mousemove(function(e) {
-    $('#brush').css({'top': e.pageY+'px', 'left': e.pageX+'px'}).addClass('active');
-    $('.brush-overlay').css({'top': e.pageY+'px', 'left': e.pageX+'px'}).addClass('active');
-    console.log("X:" + e.pageX + "   Y:" + e.pageY);
+		var cWidth = $(document).width();
+		var cHeight = $(document).height();
+		
+		$('#brush').css({'top': e.pageY+'px', 'left': e.pageX+'px'}).addClass('active');
+    // $('.brush-overlay').css({'top': e.pageY+'px', 'left': e.pageX+'px'}).addClass('active');
+    $('.brush-overlay').addClass('active');
+    $('.brush-overlay').css('transform', 'translate(' + e.pageX*100/cWidth + 'px, ' + e.pageY*100/cHeight + 'px)');
+    // $('.brush-overlay').css('transform', 'translate(' + tWidth + 'px, ' + tHeight + 'px)');
+    
+    console.log("X:" + e.pageX + "   Y:" + e.pageY + "cWidth:" + cWidth + "   cHeight" + cHeight);
   });
 
 });
